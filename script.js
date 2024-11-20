a5fb798ebae9607c3f8710e809e003feda3fb5b1
Akachukwu Okoli <abelokoli02@gmail.com>
2024-11-20 11:06:58 +0100

    updated files

--- script.js
+++ script.js
@@ -1,19 +1,19 @@
 const texts = [
   {
     headline: "Discover innovative ways to decorate",
-    description: "We provide unmatched quality, comfort, and style for property owners across the country. Our experts combine form and function in bringing your vision to life. Create a room in your own style with ourcollection and make your property a reflection of you and what you have"
+    description: "We provide unmatched quality, comfort, and style for property owners across the country. Our experts combine form and function in bringing your vision to life. Create a room in your own style with ourcollection and make your property a reflection of you and what you have" 
   },
   {
     headline: "Transform your living space effortlessly",
-    description: "Choose from our diverse collection to create a space you'll love."
+    description: "Choose from our diverse collection to create a space you'll love.Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ut eveniet ipsam voluptatibus animi facere iusto, adipisci aliquid dolore deserunt saepe. Deserunt soluta pariatur placeat minima. Excepturi veniam iste eos odio."
   },
   {
     headline: "Style meets functionality",
-    description: "Our furniture is designed to provide comfort and elegance."
+    description: "Our furniture is designed to provide comfort and elegance. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ut eveniet ipsam voluptatibus animi facere iusto, adipisci aliquid dolore deserunt saepe. Deserunt soluta pariatur placeat minima. Excepturi veniam iste eos odio."
   },
   {
     headline: "Make your space truly yours",
-    description: "Personalize your rooms with innovative designs."
+    description: "Personalize your rooms with innovative designs. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ut eveniet ipsam voluptatibus animi facere iusto, adipisci aliquid dolore deserunt saepe. Deserunt soluta pariatur placeat minima. Excepturi veniam iste eos odio."
   }
 ];
 
